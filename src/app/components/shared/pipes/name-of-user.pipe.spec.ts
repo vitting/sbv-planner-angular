4c5152cec0b1b7f9f8c142c55af889fe5b1f7174
Christian Nicolaisen <cvn_vitting@hotmail.com>
2019-09-01 19:52:43 +0200

    removede unused pipe

--- src/app/components/shared/pipes/name-of-user.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { NameOfUserPipe } from './name-of-user.pipe';
-
-describe('NameOfUserPipe', () => {
-  it('create an instance', () => {
-    const pipe = new NameOfUserPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
